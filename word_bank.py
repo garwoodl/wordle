a8505283ae85485752acc3464d942fca2403967f
Logan Garwood <126724039+garwoodl@users.noreply.github.com>
2024-06-14 19:17:45 +0000

    inits

--- word_bank.py
+++ word_bank.py
@@ -3,13 +3,28 @@ File to handle the WordBank class which makes WordBank objects consisting of
 all possible legal words and has functions to get legal moves
 """
 
+import csv
+
+ALPHABET = set(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+                'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
+
 class WordBank:
   def __init__(self, filename=''):
     self.filename=filename
+    self.word_set = set()
+
+    # load the file into the word set
+    with open(self.filename, mode='r') as f:
+      the_file = csv.reader(f)
+      for lines in the_file:
+        self.word_set.add(lines[0].lower())
 
 
 def main():
-  ...
+  test_bank = WordBank(filename='test_word_bank.csv')
+  print(test_bank.word_set)
+
 
 if __name__ == "__main__":
   main()
