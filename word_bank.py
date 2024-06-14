64efb3182bf2759b4d660d8f12e8b6b1cb41a701
Logan Garwood <126724039+garwoodl@users.noreply.github.com>
2024-06-14 12:03:07 -0700

    Create word_bank.py

---
+++ word_bank.py
@@ -0,0 +1,15 @@
+"""
+File to handle the WordBank class which makes WordBank objects consisting of
+all possible legal words and has functions to get legal moves
+"""
+
+class WordBank:
+  def __init__(self, filename=''):
+    self.filename=filename
+
+
+def main():
+  ...
+
+if __name__ == "__main__":
+  main()
