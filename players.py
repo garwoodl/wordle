88479d6b1354b1b644f7eea63dcf78ac657199f8
Logan Garwood <126724039+garwoodl@users.noreply.github.com>
2024-06-15 00:44:13 +0000

    working pretty well now

--- players.py
+++ players.py
@@ -3,15 +3,51 @@ File to contain all of human and bot players
 Inspo taken from: https://github.com/Kinkelin/WordleCompetition/blob/main/WordleAI.py#L12
 '''
 from abc import ABC, abstractmethod
-from word_bank import WordBank
 import random
+import config
+
+
+def remaining_options(words, guess_history):
+    '''
+    Guesses is a list of tuples (word, result)
+    Returns the subset of words that fit the parameters
+    '''
+    in_word = set()  # set of characters
+    right_place = set()  # set of tuples (char, index)
+    wrong_place = set()  # set of tuples (char, index)
+    not_in_word = set()  # set of characters
+
+    for word, result in guess_history:
+        for i, letter in enumerate(word):
+            if result[i] == config.GREEN:
+                in_word.add(letter)
+                right_place.add((letter, i))
+            elif result[i] == config.YELLOW:
+                in_word.add(letter)
+                wrong_place.add((letter, i))
+            else:
+                not_in_word.add(letter)
+    # in case there were multiple occurrences of a letter
+    not_in_word = not_in_word.difference(in_word)
+
+    # filter words
+    for c in in_word:
+        words = [w for w in words if c in w]
+    for c in not_in_word:
+        words = [w for w in words if c not in w]
+    for c in right_place:
+        words = [w for w in words if c[0] == w[c[1]]]
+    for c in wrong_place:
+        words = [w for w in words if c[0] != w[c[1]]]
+
+    return set(words)
 
 class Player:
-    def __init__(self, wb: WordBank, name: str):
+    def __init__(self, words: set[str], name: str):
         '''
         Takes in a WordBank object wb and a name to identify the player
         '''
-        self.wb = wb
+        self.words = words
         self.name = name
     
     @abstractmethod
@@ -30,4 +66,20 @@ class Player:
 
 class RandomPlayer(Player):
     def guess(self, guess_history):
-        return random.choice(self.wb.word_set)
+        available_words = list(remaining_options(self.words, guess_history))
+        return random.choice(available_words)
+    
+class HumanPlayer(Player):
+    def guess(self, guess_history):
+        while True:
+            try:
+                guess = input("Enter a word: ")
+                guess = guess.lower()
+                if guess == 'q':
+                    return False
+                if guess in self.words:
+                    return guess
+                else:
+                    print("That is not a valid word. Try again...")
+            except:
+                print("Invalid input. Try again...")
--- wordle_game.py
+++ wordle_game.py
@@ -1,32 +1,46 @@
 '''
 Host games and manage the board in the Game class
 '''
-from word_bank import WordBank
 import config
-from players import *
+from players import Player
 
-class Game:
-    def __init__(self, wb: WordBank, word: str):
-        '''
-        guesses is a list of tuples (word, result)
-        where each word is a string and each result is a string of chars
-        representing the colored squares
-        '''
-        self.word_set = wb.word_set
-        self.word = word
-        self.words_guessed = []  # list of words guessed
-        self.guess_history = []  # list of tuples of (word, result)
-        self.success = False
-
-    def guess_is_legal(self, guess):
-        return len(guess) == 5 and guess in self.word_set
+def guess_is_legal(words, guess):
+    return len(guess) == 5 and guess in words
     
-    def play_game(self, player: Player, verbose=True):
-        '''
-        Runs a game with the given player
-        Returns (success, guesses) where success iff game won
-        and guesses is a list of the words guessed
-        '''
+def play_game(words, word, player: Player, verbose=True):
+    '''
+    Runs a game with the given player, legal_words (words), and target word (word)
+    Returns (success, words_guessed) where success iff game won
+    and guesses is a list of the words guessed
+    '''
+    # inspo from https://github.com/Kinkelin/WordleCompetition/blob/main/Competition.py#L36
+    guess_history = []
+    words_guessed = []
+    for i in range(6):  # six guesses
+        guess = player.guess(guess_history)
+        if not guess_is_legal(words, guess):
+            if verbose:
+                print(f'Illegal guess!')
+                print(f'{player.name} cannot guess {guess}.')
+            return False, words_guessed
+        
+        result = get_result(word, guess)
+        words_guessed.append(guess)
+        guess_history.append((guess, result))
+
+        if guess == word:
+            if verbose:
+                print("Good Job!")
+                print(f"{player.name} correctly guessed {word} in {i+1} guesses!")
+            return True, words_guessed
+    return False, words_guessed
+
+def txt_to_set(filename):
+    word_set = set()
+    with open(filename, mode='r') as file:
+      for line in file:
+        word_set.add(line.strip().lower())
+    return word_set
 
 
 def get_result(true_word: str, guess_word: str) -> str:
@@ -34,29 +48,34 @@ def get_result(true_word: str, guess_word: str) -> str:
     Returns a string of color characters for the given guessed word
     Assumes true_word and guess_word are same length
     '''
-    result = ''
+    result = [config.GRAY] * 5
+    # make a dict to keep track of multiple letter occurrences
+    true_counts = {}
+    for c in true_word:
+        if c in true_counts:
+            true_counts[c] += 1
+        else:
+            true_counts[c] = 1
+
+    # iterate once to get all greens
     for i, letter in enumerate(guess_word):
         if letter == true_word[i]:
-            result += config.GREEN
-        elif letter in true_word:
-            result += config.YELLOW
-        else:
-            result += config.GRAY
-    return result
+            result[i] = config.GREEN
+            true_counts[letter] -= 1
+    # iterate again to get the yellows
+    for i, letter in enumerate(guess_word):
+        if ((letter in true_word) and (result[i] == config.GRAY) and 
+            (true_counts[letter] > 0)):
+            result[i] = config.YELLOW
+            true_counts[letter] -= 1  # make sure not to overdisplay yellows
+        
+    return ''.join(result)
 
 
 def main():
-    true_word = 'smell'
-    wb = WordBank('official_word_bank.txt')
-    G = Game(wb, true_word)
-    
-    guessed_words = ['soare', 'elend', 'gecko']
-    guesses = [(w, get_result(true_word, w)) for w in guessed_words]
-    available_words = G.word_bank.remaining_options(guesses)
-    print(f'The word to guess is {G.word}')
-    for word in guessed_words:
-        print(f'{word}\t\t{get_result(G.word, word)}')
-    print(available_words)
+    word = 'grece'
+    filename = 'official_word_bank.txt'
+    words = txt_to_set(filename)
 
 
     
